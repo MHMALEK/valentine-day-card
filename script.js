7edead9361d7a31ac05daa57c4a31ca05dec38e2
Mohammad Malek <mhos.malek@gmail.com>
2024-02-13 16:14:37 +0100

    add some new functions

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 window.onload = function () {
+  // Fetch random cat image
+  fetchCatImage();
+};
+
+function fetchCatImage() {
   var request = new XMLHttpRequest();
   request.open(
     "GET",
@@ -9,7 +14,6 @@ window.onload = function () {
   request.onload = function () {
     if (this.status >= 200 && this.status < 400) {
       var data = JSON.parse(this.response);
-      console.log(this.response);
       document.getElementById("catImage").src = data[0].url;
     } else {
       console.log("Server returned an error");
@@ -21,4 +25,35 @@ window.onload = function () {
   };
 
   request.send();
-};
+}
+
+function translateText() {
+  var languages = ["es", "fr", "de", "it", "ja"];
+  var randomLanguage = languages[Math.floor(Math.random() * languages.length)];
+
+  var text = "Welcome to the random cat image page!";
+  var request = new XMLHttpRequest();
+  request.open(
+    "GET",
+    `https://translation.googleapis.com/language/translate/v2?key=YOUR_API_KEY&q=${encodeURIComponent(
+      text
+    )}&target=${randomLanguage}`,
+    true
+  );
+
+  request.onload = function () {
+    if (this.status >= 200 && this.status < 400) {
+      var data = JSON.parse(this.response);
+      document.getElementById("welcomeText").innerText =
+        data.data.translations[0].translatedText;
+    } else {
+      console.log("Translation server returned an error");
+    }
+  };
+
+  request.onerror = function () {
+    console.log("There was a connection error");
+  };
+
+  request.send();
+}
